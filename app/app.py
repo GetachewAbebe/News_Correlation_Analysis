c0ca7482aba71ad03895bd58674525685b1417d9
Getachew Abebe <gechachin@gmail.com>
2024-04-13 16:59:29 +0300

    Screenshots and Reqiurment

--- app/app.py
+++ app/app.py
@@ -44,4 +44,31 @@ ax.set_ylabel('Number of media')
 ax.tick_params(axis='x', rotation=45)
 st.pyplot(fig)
 
+st.subheader("Media List")
 
+file_path2 = 'C:\\Users\\gecha\\OneDrive\\Documents\\news_correlation_10ac_week0\\Data\\domains_location.csv'
+file_path2 = file_path2.replace("\\", "/")
+location_df = pd.read_csv(file_path2)
+
+location_df
+
+
+
+# Create some sample data (replace with your cleaned data)
+data = {'source_name': ['Reuters', 'BBC News', 'CNN', 'New York Times', 'Reuters', 'BBC News', 'CNN', 'Fox News']}
+df = pd.DataFrame(data)
+
+# Perform a count of news articles by source
+source_counts = df['source_name'].value_counts()
+
+# Get the source with the largest count of news articles
+largest_source = source_counts.idxmax()
+largest_count = source_counts.max()
+
+
+# Display top 10 sources
+st.subheader("Top 10 Websites with Most Articles")
+st.dataframe(top_10_sources)
+
+# You can add charts here as well using libraries like matplotlib or plotly
+# st.bar_chart(top_10_sources)  # Example bar chart of top 10 sources
